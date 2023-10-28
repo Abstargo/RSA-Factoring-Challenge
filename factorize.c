5f61ba639fb7719376dcc329a2d5e874f0c46428
Yassir <james.iharo@gmail.com>
2023-10-28 13:57:37 +0100

    RSA Factoring

---
+++ factorize.c
@@ -0,0 +1,22 @@
+#include "factor.h"
+#include <stdint.h>
+
+int factorize(char *buffer)
+{
+
+    uint32_t num;
+	uint32_t i;
+
+    num = atoi(buffer);
+
+    for (i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            printf("%d=%d*%d\n", num, num/i, i);
+            break;
+        }
+    }
+
+    return (0);
+}
